679a737711181b4e3aedb00a184f6630e8870a75
Tony Antelman <AntelmanAnthonyJ@JohnDeere.com>
2020-03-05 15:12:49 -0600

    changed the default tile background image name to follow proper naming conventions, and added functionalty to log an object populated with tile info when it is clicked.

--- src/Tile.js
+++ src/Tile.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-import Axios from 'axios'
 
 class Tile extends Component {
-    
+    // Eventually, this handle click will flip the card and allow for the game logic to be used.
     handleClick = (currentTile) => {
         console.log('handleClick')
         console.log(currentTile)
-
     }
+    // There will need to be a second handle click for a new button(div) that will contain an "INFO" icon in the corner that will trigger the BIO of the character. BUT ONLY WHEN IT'S FLIPPED OVER!
+
+
 
     render(){
         console.log('Tile Component Render')
@@ -18,25 +19,33 @@ class Tile extends Component {
 
         let tiles = []
 
+        //This logic needs to be improved to use an array or object with list of comicbook covers that represent the character, and then create two tiles per cover, and then randomize the covers and pass them to the tile div.
         for (let i=0;i<20;i++){
-            let currentTile =  `Tile${i+1}`
-            let currentComic = (this.props.characters && this.props.characters.id)
+            let currentCharacterId = (this.props.characters && this.props.characters.id)
             let currentCharacter = (this.props.characters && this.props.characters.name)
             let currentBackgroundImg = (this.props.comic && `${this.props.comic.thumbnail.path}.${this.props.comic.thumbnail.extension}`)
-            console.log(currentBackgroundImg)
+            let defaultBackgroundImg = '/images/tileback.jpeg'
+            let currentTile =  {
+                tileId: `Tile${i+1}`, 
+                currentCharacter: currentCharacter, 
+                currentCharacterId: currentCharacterId, 
+                currentBackgroundImg: currentBackgroundImg
+            }
+
+            // console.log(currentBackgroundImg)
             tiles.push(
                 <div 
                     className='tile' 
                     key={i} 
                     onClick={() => this.handleClick(currentTile)}
                     style={{ 
-                        backgroundImage:  `url(${currentBackgroundImg})`,
+                        backgroundImage:  `url(${defaultBackgroundImg})`,
                         backgroundSize: `cover`, 
                         backgroundPosition: `center center`
                     }}
                     >
-                    {currentComic}
-                    <p>{currentCharacter}</p>
+                    {/* {currentComic} */}
+                    {/* <p>{currentCharacter}</p> */}
                 </div>)
         }
         return(
