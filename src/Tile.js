7f0234c404f70cdf8e6de687440b07297920a512
Tony Antelman <AntelmanAnthonyJ@JohnDeere.com>
2020-03-06 14:25:57 -0600

    added ability to click on a card and flip to a different image

--- src/Tile.js
+++ src/Tile.js
@@ -2,12 +2,25 @@
 import React, { Component } from 'react';
 
 class Tile extends Component {
+
+    state = {
+        bgImg: '/images/tileback.jpeg'
+    }
+
     // Eventually, this handle click will flip the card and allow for the game logic to be used.
     handleClick = (currentTile) => {
         console.log('handleClick')
         console.log(currentTile)
         // console.log(this.props)
         console.log(currentTile.currentBackgroundImg)
+        currentTile.currentBackgroundImg = '/images/sample-iron-man.jpg'
+        console.log(currentTile.currentBackgroundImg)
+        let curBgImg = document.querySelector(`.${currentTile.tileId}`)
+        console.log(curBgImg)
+        curBgImg.style.backgroundImage = `url('/images/sample-iron-man.jpg')`
+        // this.setState({
+        //     bgImg: '/images/sample-iron-man.jpg'
+        // })
     }
     // There will need to be a second handle click for a new button(div) that will contain an "INFO" icon in the corner that will trigger the BIO of the character. BUT ONLY WHEN IT'S FLIPPED OVER!
 
@@ -15,7 +28,6 @@ class Tile extends Component {
 
     render(){
         console.log('Tile Component Render')
-        // console.log(this.props.characters.comics.items)
 
         let tiles = []
         let tileCount = 10
@@ -23,31 +35,47 @@ class Tile extends Component {
         // creates an array of different comic book paths to use in an axios call
         const characterComicURIs = this.props.characters.comics.items.map((cover) => {
             return cover.resourceURI
-        }) //this works
-        // console.log(characterComicURIs) //returns list of URIs correctly
-        // console.log(characterComicURIs[0])
+        })
 
-        const shuffle = function (array) {
-            let currentIndex = array.length
-            let tempValue, randomIndex
-
-            while ( 0 !== currentIndex) {
-                randomIndex = Math.floor(Math.random()*currentIndex)
-                currentIndex -=1
-                tempValue = array[randomIndex]
-                array[randomIndex] = tempValue
+        //This function is used to double the comics in the array
+        const doubleArray = function(array){
+            let tempArray = []
+            for (let i=0;i<array.length;i++){
+                for (let j=0;j<2;j++){
+                    tempArray.push(array[i])
+                }
             }
-            return array
+            return tempArray
         }
-        // console.log(shuffle(characterComicURIs))
-        let shuffledURIs = shuffle(characterComicURIs)
 
-        //This logic needs to be improved to use an array or object with list of comicbook covers that represent the character, and then create two stiles per cover, and then randomize the covers and pass them to the tile div.
+        //This function is used to shuffle the values in the array in a random order. Fisher–Yates Shuffle
+        const shuffle = function (array) {
+            let m = array.length, t, i;
+
+                // While there remain elements to shuffle…
+                while (m) {
+
+                // Pick a remaining element…
+                i = Math.floor(Math.random() * m--);
+
+                // And swap it with the current element.
+                t = array[m];
+                array[m] = array[i];
+                array[i] = t;
+            }
+            return array;
+        }
+
+        // in this, we have a rather large list of values, and we need to pair it down to be limited to the defined number of values. since the tile count is set above, we will reference that. 
+        let comicsDoubled = doubleArray(characterComicURIs.slice(0,tileCount))
+        // now that we have an array that contains the number of values we want, now it can be shuffled. 
+        let shuffledURIs = shuffle(comicsDoubled)
+
+
+        // this section assigns a bunch of variables, we then push each value into a tiles array to be oplulated in the GameTiles component.
         for (let i=0;i<tileCount;i++){
             let currentCharacterId = (this.props.characters && this.props.characters.id)
             let currentCharacter = (this.props.characters && this.props.characters.name)
-            // let currentBackgroundImg = (this.props.comic && `${this.props.comic.thumbnail.path}.${this.props.comic.thumbnail.extension}`) // this works, but it's static. 
-            // instead of the above, I need to use the shuffle function and pull a value from the index.
             let currentBackgroundImg = shuffledURIs[i]
             let defaultBackgroundImg = '/images/tileback.jpeg'
             let currentTile =  {
@@ -56,22 +84,15 @@ class Tile extends Component {
                 currentCharacterId: currentCharacterId, 
                 currentBackgroundImg: currentBackgroundImg
             }
-
-            //shuffle tiles array.
-
-            // console.log(currentBackgroundImg)
             tiles.push(
-                <div className='flex-cell'>
+                <div key={`Tile${i+1}`} className='flex-cell'>
                     <div 
-                        className='flex-item' 
-                        key={i} 
+                        className={`flex-item ${currentTile.tileId}`}
                         onClick={() => this.handleClick(currentTile)}
                         style={{ 
-                            backgroundImage:  `url(${defaultBackgroundImg})`
+                            backgroundImage:  `url(${this.state.bgImg})`
                         }}
                         >
-                        {/* {currentComic} */}
-                        {/* <p>{currentCharacter}</p> */}
                     </div>
                 </div>)
         }
